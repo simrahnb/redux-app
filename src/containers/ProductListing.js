0d49d561949655a851e64cd5a433f1d61d7fd56b
simrahnb <65752120+simrahnb@users.noreply.github.com>
2023-03-02 14:52:44 +0500

    used axios to make api call

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import ProductComponent from "./ProductComponent";
+import axios from "axios";
 
 const ProductListing = () => {
   const products = useSelector((state) => state);
-  console.log(products);
+
+  const fetchProducts = async () => {
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => {
+        console.log("Error:", err);
+      });
+    console.log(response.data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div className="container">
       <ProductComponent />
